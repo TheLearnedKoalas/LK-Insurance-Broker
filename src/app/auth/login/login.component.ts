92e572aff886294a348a8746fbff81591f4db33e
ArnaudovSt <telerikacademy2016@gmail.com>
2017-09-09 00:20:35 +0300

    login func

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from '../shared/user.service';
 
 @Component({
-  selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+  email: string;
+  password1: string;
 
-  constructor() { }
+  constructor(private userService: UserService, private router: Router) { }
 
-  ngOnInit() {
+  login() {
+    this.userService.login(this.email, this.password1);
+    this.userService.verifyUser();
   }
 
+  signup() {
+    this.router.navigate(['/auth/signup']);
+  }
+
+  cancel() {
+    this.router.navigate(['']);
+  }
 }
