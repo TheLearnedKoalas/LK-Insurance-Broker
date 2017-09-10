d3966ef972909f3a7c9358d61e348414e2664505
AsenaPencheva <asenap86@gmail.com>
2017-09-10 23:12:56 +0300

    changed form  wrong merging

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,9 @@ import { AboutComponent } from './components/about/about.component';
 import { CarouselComponent } from './components/carousel/carousel.component';
 
 import { Ng2BreadcrumbModule, BreadcrumbService } from 'ng2-breadcrumb/ng2-breadcrumb';
+import { InsuranceService } from './insurance/services/insurance.service';
+import { ClientService } from './insurance/services/client.service';
+import { VehicleService } from './insurance/services/vehicle.service';
 // import { SidebarModule } from 'ng-sidebar';
 // import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
@@ -39,7 +42,7 @@ import { Ng2BreadcrumbModule, BreadcrumbService } from 'ng2-breadcrumb/ng2-bread
     ReactiveFormsModule,
     // PopupModule
   ],
-  providers: [BreadcrumbService],
+  providers: [InsuranceService, ClientService, VehicleService, BreadcrumbService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/insurance/forms/client-form/client-form.component.ts
+++ src/app/insurance/forms/client-form/client-form.component.ts
@@ -1,35 +1,42 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Validator } from 'validator.ts/Validator';
+import { Client } from './../../models/client';
+import { Component, OnInit, Input, Output, EventEmitter, ChangeDetectionStrategy } from '@angular/core';
 
 import { ClientService } from '../../services/client.service';
+import { Validators, FormGroup, FormControl, FormGroupName, ControlContainer } from '@angular/forms';
 
 @Component({
   selector: 'app-client-form',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './client-form.component.html',
   styleUrls: ['../insurance-form/insurance-form.component.css']
 })
 export class ClientFormComponent implements OnInit {
-
-
+  @Output()
+  loadContent = new EventEmitter<object>();
   clientTypes = [{ name: 'физическо лице' }, { name: 'юридическо лице' }];
+
+  public clientType = new FormControl(this.clientTypes[0].name, [Validators.required]);
+
+  public name = new FormControl('', []);
+
+  public id= new FormControl('', []);
+
+  public adress = new FormControl('', []);
+
+  public client: FormGroup;
 
   constructor(private clientService: ClientService) { }
 
-  @Input()
-  client = {
-    clientType: '',
-    id: '',
-    name: '',
-    adress: '',
-  };
-
-  @Output()
-  loadContent = new EventEmitter<object>();
+  // @Output()
+  // loadContent = new EventEmitter<object>();
 
   CreateClient() {
-    // this.clientService.createClient(this.client)
-    //   .subscibe(res => {
-    //     this.loadContent.emit(res);
-    //   });
+    // console.log(this.client.value);
+    this.clientService.create(this.client.value)
+      .subscribe(res => {
+        this.loadContent.emit(this.client.value);
+      });
   }
 
   FindClient() {
@@ -37,6 +44,12 @@ export class ClientFormComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.client = new FormGroup({
+      clientType: this.clientType,
+      name: this.name,
+      id: this.id,
+      adress: this.adress
+    });
   }
 
 }
